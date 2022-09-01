6bd14cb2d3da83870827666bca457b8e80ef6496
Davit <hovsepyan.davit93@gmail.com>
2022-09-01 20:31:16 +0400

    initial commit

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var conditionImageView: UIImageView!
+    @IBOutlet var tempretureLable: UILabel!
+    @IBOutlet var conditionLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func lockationButton(_ sender: Any) {
+    }
+    @IBAction func lockationButtonPrest(_ sender: Any) {
+    }
+    
 }
 
